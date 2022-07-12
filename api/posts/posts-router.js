7477361ff09c984636de2e4b0b2875386762ea39
RealSadLemon <esmodearburk@gmail.com>
2022-07-12 18:13:51 -0500

    Set up functionality for GET by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -8,8 +8,16 @@ const router = express.Router();
 router.get('/', (req, res) => {
     PostsModel.find()
         .then(posts => {
-            res.status(200).json(posts)
-        })
-})
+            res.status(200).json(posts);
+        });
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    PostsModel.findById(id)
+        .then(post => {
+            res.status(200).json(post);
+        });
+});
 
 module.exports = router;
