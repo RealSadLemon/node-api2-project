985f12b08227d277630c59c5f8c79ae05d7d07a0
RealSadLemon <esmodearburk@gmail.com>
2022-07-12 22:51:03 -0500

    Undid poor decision making of last commit, fixed bug where post wasn't returning the uploaded post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -20,55 +20,44 @@ router.get('/:id', (req, res) => {
             res.status(200).json(post);
             return;
         }
-        res.status(404).json({ message: "does not exist" })
+        res.status(404).json({ message: "The posts information could not be retrieved" })
     })
 });
 
 router.post('/', (req, res) => {
-    PostsModel.insert(req.body)
-    .then(post => {
-        if(post){
-            res.status(201).json(post);
-            return;
-        }
-        res.status(404).json({ message: "The posts information could not be retrieved" })
-    })
+    const body = req.body;
+    console.log(body)
+    PostsModel.insert(body)
+        .then(createdPost => {
+            PostsModel.findById(createdPost.id)
+                .then(post => {
+                    res.status(201).json(post);
+                })
+        })
 })
 
 router.put('/:id', (req, res) => {
     const { id } = req.params;
     PostsModel.update(id, req.body)
-    .then(post => {
-        if(post){
-            res.status(200).json(post);
-            return;
-        }
-        res.status(404).json({ message: "The posts information could not be retrieved" })
-    })
+        .then(updatedPost => {
+            res.status(200).json(updatedPost);
+        })
 })
 
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
     PostsModel.remove(id)
-    .then(post => {
-        if(post){
-            res.status(200).json(post);
-            return;
-        }
-        res.status(404).json({ message: "The posts information could not be retrieved" })
-    })
+        .then(deletedPost => {
+            res.status(200).json(deletedPost);
+        })
 })
 
 router.get('/:id/comments', (req, res) => {
     const { id } = req.params;
     PostsModel.findPostComments(id)
-    .then(post => {
-        if(post){
-            res.status(200).json(post);
-            return;
-        }
-        res.status(404).json({ message: "The posts information could not be retrieved" })
-    })
+        .then(comments => {
+            res.status(200).json(comments)
+        })
 })
 
 module.exports = router;
